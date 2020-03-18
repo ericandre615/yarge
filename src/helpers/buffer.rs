fcc486e85bdee213e6323c3aede15cdeb9c7b084
Eric Andre <ericandre615@gmail.com>
2020-03-18 18:38:14 -0500

    WIP start implementing FrameBuffers for rendering post-process effects

--- src/helpers/buffer.rs
+++ src/helpers/buffer.rs
@@ -178,3 +178,155 @@ impl Drop for VertexArray {
         }
     }
 }
+
+#[derive(Debug)]
+pub struct FrameBuffer {
+    fbo: gl::types::GLuint,
+    texture: FrameBufferTexture,
+    _marker: ::std::marker::PhantomData<B>,
+}
+
+impl FrameBuffer {
+    pub fn new(screen_width: u32, screen_height: u32) -> Result<FrameBuffer, Error> {
+        let mut fbo: gl::types::GLuint =  0;
+        let texture = FrameBufferTexture::new(screen_width, screen_height);
+
+        unsafe {
+            gl::GenFramebuffers(1, &fbo);
+            gl::BindFramebuffer(gl::FRAMEBUFFER, fbo);
+            gl::FramebufferTexture2D(gl::FRAMEBUFFER, gl::COLOR_ATTACHMENT0, gl::TEXTURE_2D, texture, 0);
+            // TODO: might need this later? for depth-testing/3D
+            // but also probably want it to be a RenderBuffer or FrameBuffer<RenderBuffer>
+            //gl::FramebufferRenderbuffer(gl::FRAMEBUFFER, gl::DEPTH_ATTACHMENT, gl::RENDERBUFFER, rbo_depth);
+
+            if gl::CheckFramebufferStatus(gl::FAMEBUFFER) != gl::FRAMEBUFFER_COMPLETE {
+                return Err("Failed to create FrameBuffer");
+            }
+
+            gl::BindFramebuffer(gl::FRAMEBUFFER, 0)
+        }
+
+        Ok(FrameBuffer {
+            fbo,
+            texture,
+            _marker: ::std::marker::PhantomData,
+        })
+    }
+
+    pub fn bind(&self) {
+        unsafe {
+            gl::BindFramebuffer(gl::FRAMEBUFFER, self.fbo);
+        }
+    }
+
+    pub fn unbind(&self) {
+        unsafe {
+            gl::BindFramebuffer(gl::FRAMEBUFFER, 0);
+        }
+    }
+}
+
+impl Drop for FrameBuffer {
+    fn drop(&mut self) {
+        unsafe {
+            gl::DeleteFramebuffers(1, &mut self.fbo);
+        }
+    }
+}
+
+#[derive(Debug)]
+struct FrameBufferTexture {
+    texture_handle: gl::types::GLuint,
+
+}
+
+impl FrameBufferTexture {
+    pub fn new(screen_width: u32, screen_height: u32) -> FrameBufferTexture {
+        let texture_handle = create_fb_texture(screen_width, screen_height);
+
+        FrameBufferTexture {
+            texture_handle,
+        }
+    }
+
+    pub fn get_texture_handle(&self) -> gl::types::GLuint {
+        self.texture_handle
+    }
+
+    pub fn set_size(&self, width: u32, height: u32) {
+        self.bind();
+        unsafe {
+            gl::TexImage2D(
+                gl::TEXTURE_2D,
+                0,
+                gl::RGBA as i32,
+                width as i32,
+                height as i32,
+                0,
+                gl::RGBA,
+                gl::UNSIGNED_BYTE,
+                std::ptr::null() //NULL
+            );
+        }
+        self.unbind();
+    }
+
+    pub fn bind(&self) {
+        unsafe {
+            gl::BindTexture(gl::TEXTURE_2D, self.texture_handle);
+        }
+    }
+
+    pub fn bind_to_unit(&self, slot: u32) {
+        unsafe {
+            gl::ActiveTexture(gl::TEXTURE0 + slot as gl::types::GLuint);
+            gl::BindTexture(gl::TEXTURE_2D, self.texture_handle);
+        }
+    }
+
+    pub fn unbind(&self) {
+        unsafe {
+            gl::BindTexture(gl::TEXTURE_2D, 0);
+        }
+    }
+}
+
+// TODO: this is very much the same as in Texture minus image and a couple of different options
+// most likely will move to use Texture, but be able to create a Texture::new without an image
+// and also taking in TextureSettings
+fn create_fb_texture(screen_width: u32, screen_height: u32) -> gl::types::GLuint {
+    let mut texture_handle: gl::types::GLuint = 0;
+
+    gl::ActiveTexture(gl::TEXTURE0);
+    gl::GenTextures(1, &texture_handle);
+    gl::BindTexture(gl::TEXTURE_2D, texture_handle);
+    gl::TexParameteri(gl::TEXTURE_2D, gl::TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE as i32);
+    gl::TexParameteri(gl::TEXTURE_2D, gl::TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE as i32);
+    gl::TexParameteri(gl::TEXTURE_2D, gl::TEXTURE_MAG_FILTER, gl::LINEAR as i32);
+    gl::TexParameteri(gl::TEXTURE_2D, gl::TEXTURE_MIN_FILTER, gl::LINEAR as i32);
+
+    gl::TexImage2D(
+        gl::TEXTURE_2D,
+        0,
+        gl::RGBA,
+        screen_width as i32,
+        screen_height as i32,
+        0,
+        gl::RGBA,
+        gl::UNSIGNED_BYTE,
+        std::ptr::null() // NULL // 0? null ptr? this is never clear around here
+    );
+
+    gl::BindTexture(GL_TEXTURE_2D, 0);
+
+    texture_handle
+}
+
+impl Drop for FrameBufferTexture {
+    fn drop(&mut self) {
+        unsafe {
+            gl::DeleteTextures(1, &mut self.texture_handle);
+        }
+    }
+}
+
---
+++ src/renderer/render_target.rs
@@ -0,0 +1,117 @@
+use crate::helpers::buffer::{FrameBuffer, ArrayBuffer, VertexBuffer};
+use crate::resources::{Resources};
+use crate::helpers::shaders{Program};
+use crate::helpers::data;
+
+#[derive(VertexAttribPointers)]
+#[derive(Debug)]
+#[repr(C, packed)]
+pub struct RenderVertex {
+    #[location=0]
+    pos: data::f32_f32,
+    #[location=1]
+    uv: data::f32_f32,
+}
+
+#[derive(Debug)]
+pub struct RenderTarget {
+    vbo: ArrayBuffer,
+    vao: VertexArray,
+    pub program: Option<Program>,
+    pub frame_buffer: FrameBuffer,
+    vertices: Vec<RenderVertex>,
+}
+
+impl RenderTarget {
+    pub fn new(screen_width: u32, screen_height: u32) -> RenderTarget {
+        let frame_buffer = FrameBuffer::new(screen_width, screen_height);
+        let vbo = ArrayBuffer::new();
+        let vao = VertexBuffer::new();
+
+        let vertices = vec![
+            RenderVertex { pos: (-1.0, 1.0).into(), uv: (0.0, 1.0).into() },
+            RenderVertex { pos: (-1.0, -1.0).into(), uv: (0.0, 0.0).into() },
+            RenderVertex { pos: (1.0, -1.0).into(), uv: (1.0, 0.0).into() },
+
+            RenderVertex { pos: (-1.0, 1.0).into(), uv: (0.0, 1.0).into() },
+            RenderVertex { pos: (1.0, -1.0).into(), uv: (1.0, 0.0).into() },
+            RenderVertex { pos: (1.0, 1.0).into(), uv: (1.0, 1.0).into() },
+        ];
+
+        vbo.bind();
+        vbo.static_draw_data(&vertices);
+        vbo.unbind();
+
+        vao.bind();
+        vbo.bind();
+
+        RenderVertex::vertex_attrib_pointers();
+
+        vbo.unbind();
+        vao.unbind();
+
+        RenderTarget {
+            frame_buffer,
+            vbo,
+            vao,
+            vertices,
+            program: None,
+        }
+    }
+
+    pub fn set_program(&self mut, program: Program) {
+        self.program = Some(program);
+    }
+
+    pub fn render(&mut self) {
+        let program = match self.program {
+            Some(prg) => prg,
+            None => default_program(),
+        };
+    }
+}
+
+fn default_program() -> Program {
+    let vert_src = r#"
+        #version 330 core
+
+        layout (location = 0) in vec2 Position;
+        layout (location = 1) in vec2 UV;
+
+        out VS_OUTPUT {
+            vec2 UV;
+        } OUT;
+
+        void main() {
+            gl_Position = vec4(Position.x, Position.y, 0.0, 1.0);
+            OUT.UV = UV;
+        }
+    "#;
+    let frag_src = r#"
+        #version 330 core
+
+        precision mediump float;
+
+        uniform sampler2D RenderTexture;
+
+        in VS_OUTPUT {
+            vec2 UV;
+        } IN;
+
+        out vec4 Color;
+
+        void main() {
+
+            Color = texture(RenderTexture, UV);
+        }
+    "#;
+
+    let shaders = vec![
+        helpers::Shader::from_raw(&vert_src, gl::VERTEX_SHADER)?,
+        helpers::Shader::from_raw(&frag_src, gl::FRAGMENT_SHADER)?,
+    ];
+    let program = helpers::Program::from_shaders(&shaders[..], "internal/shaders/render_target")
+        .expect("Failed to load FB RenderTarget Shader Program");
+
+    program
+}
